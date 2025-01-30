1f1b92678c099e0c1bf941cf1c7f8074fa1e8e05
Erica <you@example.com>
2025-01-30 12:04:54 +0100

    table and tests 50%

---
+++ src/test/java/dev/erica/loop/MultiplicationTableTest.java
@@ -0,0 +1,32 @@
+package dev.erica.loop;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.Test;
+
+public class MultiplicationTableTest {
+    @Test
+    void testGenTable() {
+        MultiplicationTable table = new MultiplicationTable(5);
+        List<String> result = table.genTable();
+
+        List<String> expected = Arrays.asList(
+            "5 x 1 = 5",
+            "5 x 2 = 10",
+            "5 x 3 = 15",
+            "5 x 4 = 20",
+            "5 x 5 = 25",
+            "5 x 6 = 30",
+            "5 x 7 = 35",
+            "5 x 8 = 40",
+            "5 x 9 = 45",
+            "5 x 10 = 50"
+        );
+
+        assertThat(result, is(expected));
+    }
+
+}
