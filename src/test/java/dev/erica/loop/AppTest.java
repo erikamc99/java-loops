9755212aba978b889a640cddb4fe04ab32e99722
Erica <you@example.com>
2025-01-30 15:13:57 +0100

    testApp with JUnit

--- src/test/java/dev/erica/loop/AppTest.java
+++ src/test/java/dev/erica/loop/AppTest.java
@@ -1,18 +1,13 @@
 package dev.erica.loop;
-
+/* 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
     void testApp() {
         assertEquals(1, 1);
@@ -37,5 +32,39 @@ class AppTest {
                                 "5 x 10 = 50\n";
 
         assertEquals(expectedOutput.replace("\n", System.lineSeparator()), outContent.toString());
+    }
+}
+*/
+// Segundo intento con JUnit
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void testApp() {
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        App.main(new String[]{});
+
+        String[] actualOutput = outContent.toString().split(System.lineSeparator());
+        String[] expectedOutput = {
+            "5 x 1 = 5",
+            "5 x 2 = 10",
+            "5 x 3 = 15",
+            "5 x 4 = 20",
+            "5 x 5 = 25",
+            "5 x 6 = 30",
+            "5 x 7 = 35",
+            "5 x 8 = 40",
+            "5 x 9 = 45",
+            "5 x 10 = 50"
+        };
+
+        assertArrayEquals(expectedOutput, actualOutput);
     }
 }
