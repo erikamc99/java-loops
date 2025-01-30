34e0614ac1076d7c2df828874f563f25bf18ad2d
Erica <you@example.com>
2025-01-30 16:33:23 +0100

    clean comments (sorry)

--- src/test/java/dev/erica/loop/AppTest.java
+++ src/test/java/dev/erica/loop/AppTest.java
@@ -1,42 +1,5 @@
 package dev.erica.loop;
-/* 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
-class AppTest {
-
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
-    }
-
-    @Test
-    @DisplayName ("Test al mostrar en consola la tabla de multiplicar del 5")
-    void testMain() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        App.main(new String[]{});
-
-        String expectedOutput = "5 x 1 = 5\n" +
-                                "5 x 2 = 10\n" +
-                                "5 x 3 = 15\n" +
-                                "5 x 4 = 20\n" +
-                                "5 x 5 = 25\n" +
-                                "5 x 6 = 30\n" +
-                                "5 x 7 = 35\n" +
-                                "5 x 8 = 40\n" +
-                                "5 x 9 = 45\n" +
-                                "5 x 10 = 50\n";
-
-        assertEquals(expectedOutput.replace("\n", System.lineSeparator()), outContent.toString());
-    }
-}
-*/
-// Segundo intento con JUnit
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
